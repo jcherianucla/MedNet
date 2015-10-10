d8bc89040842f1bdaaebcafc0ca304b824f1e151
Matthew Allen Lin <Darthpwner@calvisitor-10-105-139-24.CalVisitor.1918.Berkeley.EDU>
2015-10-10 11:34:13 -0700

    Omar added hella features with Myo

---
+++ MedNetUnity/Assets/Myo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Myo : MonoBehaviour {
+
+	public GameObject myo;
+	public GameObject brain;
+	private int speed = 15;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myo.GetComponent<ThalmicMyo> ().pose == Thalmic.Myo.Pose.Fist && (Vector3.zero - brain.transform.localScale).sqrMagnitude < 40) {
+			brain.transform.localScale += Vector3.one * 0.05f;
+		} else if (myo.GetComponent<ThalmicMyo> ().pose == Thalmic.Myo.Pose.FingersSpread && (Vector3.zero - brain.transform.localScale).sqrMagnitude > 1)
+			brain.transform.localScale -= Vector3.one * 0.05f;
+		else if (myo.GetComponent<ThalmicMyo> ().pose == Thalmic.Myo.Pose.WaveIn)
+			brain.transform.Rotate (Vector3.up * 50 * Time.deltaTime);
+		else if (myo.GetComponent<ThalmicMyo> ().pose == Thalmic.Myo.Pose.WaveOut)
+			brain.transform.Rotate (Vector3.down * 50 * Time.deltaTime);
+	}
+}
